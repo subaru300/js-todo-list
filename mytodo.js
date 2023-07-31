23143b66b35aadbea8183b53b3573de857484862
Sergey Cherep <135381699+subaru300@users.noreply.github.com>
2023-07-31 17:32:30 +0300

    Update mytodo.js

--- mytodo.js
+++ mytodo.js
@@ -1,59 +1,96 @@
 'use strict';
 
-const taskElement = document.querySelector('.list-container');
-const newTaskInput = document.querySelector('.new-task-input');
-const addBtn = document.querySelector('.btn__add');
-const delBtn = document.querySelector('.bin-ico');
+function onPageLoaded() {
+  const newTaskInput = document.querySelector('.new-task-input');
+  const ul = document.querySelector('.todos');
+  const btnAdd = document.querySelector('.btn__add');
+  const btnSave = document.querySelector('.btn__save');
+  const btnClear = document.querySelector('.btn__clear');
 
-let taskCounter = 0;
-
-const addTask = function () {
-  const content = newTaskInput.value;
-  newTaskInput.placeholder = 'What do you have to do?';
-  if (!content) {
-    newTaskInput.placeholder = 'Write your task here!';
-    return;
-  }
-  taskCounter++;
-  const newTaskRow = `<div class="list-row">
-  <div class="list-content"> <div class="content"><input
-  type="checkbox"
-  class="custom-checkbox"
-  id="cust-check-${taskCounter}"
-  name="checkbox"
-  value="yes"
-  /><label for="cust-check-${taskCounter}"></label> <p>${content}</p></div></div></div>`;
-
-  taskElement.insertAdjacentHTML('afterbegin', newTaskRow);
-  newTaskInput.value = '';
-};
-
-const removeTask = function () {
-  const rows = document.querySelectorAll('.list-row');
-  const checkboxes = document.querySelectorAll('.custom-checkbox');
-
-  let nothingToDelete = true;
-
-  for (let i = 0; i < rows.length; i++) {
-    if (checkboxes[i].checked) {
-      rows[i].remove();
-      nothingToDelete = false;
+  function addTask() {
+    if (!newTaskInput.value) {
+      newTaskInput.placeholder = 'Write your task here!';
+      return;
     }
-    if (nothingToDelete) {
-      newTaskInput.placeholder = 'Nothing to delete!';
-      setTimeout(() => {
-        newTaskInput.placeholder = 'What do you have to do?';
-      }, 2000);
+    newTaskInput.placeholder = 'What do you have to do?';
+    const li = document.createElement('li');
+    li.classList.add('li');
+    const container = document.createElement('div');
+    container.classList.add('list-row');
+
+    const textDiv = document.createElement('div');
+    textDiv.classList.add('todo-text');
+    const newTask = newTaskInput.value;
+    textDiv.append(newTask);
+
+    const delBtn = document.createElement('div');
+    delBtn.classList.add('todo-trash');
+    const icon = document.createElement('img');
+    icon.classList.add('bin-ico');
+    icon.src = '/my-todo/bin.png';
+    delBtn.appendChild(icon);
+
+    container.append(textDiv, delBtn);
+    li.appendChild(container);
+    ul.appendChild(li);
+    ul.insertBefore(li, ul.firstChild);
+    newTaskInput.value = '';
+
+    listenDelTask(delBtn);
+  }
+
+  function listenDelTask(btn) {
+    btn.addEventListener('click', event => {
+      const container = btn.parentElement.parentElement;
+      container.remove();
+      event.stopPropagation();
+    });
+  }
+
+  btnAdd.addEventListener('click', addTask);
+
+  newTaskInput.addEventListener('keypress', keyPressed => {
+    const keyEnter = 13;
+    if (keyPressed.which == keyEnter) {
+      addTask();
+    }
+  });
+
+  function onClickTask(event) {
+    if (event.target.classList.contains('todo-text')) {
+      event.target.classList.toggle('checked');
     }
   }
-};
 
-// another way
-//     rows.forEach(function foo(e, i) {
-//       checkboxes[i].checked
-//         ? rows[i].remove()
-//         : (newTaskInput.placeholder = 'Nothing to delete!');
-//     });
+  ul.addEventListener('click', onClickTask);
 
-addBtn.addEventListener('click', addTask);
-delBtn.addEventListener('click', removeTask);
+  btnSave.addEventListener('click', () => {
+    localStorage.setItem('todos', ul.innerHTML);
+    newTaskInput.placeholder = 'Saved!';
+    setTimeout(() => {
+      newTaskInput.placeholder = 'Write your task here!';
+    }, 2000);
+  });
+  btnClear.addEventListener('click', () => {
+    ul.innerHTML = '';
+    localStorage.removeItem('todos');
+    newTaskInput.placeholder = 'Cleared!';
+    setTimeout(() => {
+      newTaskInput.placeholder = 'Write your task here!';
+    }, 2000);
+  });
+
+  function loadData() {
+    const data = localStorage.getItem('todos');
+    if (data) {
+      ul.innerHTML = data;
+    }
+    const delBtns = document.querySelectorAll('.todo-trash');
+    for (let btn of delBtns) {
+      listenDelTask(btn);
+    }
+  }
+  loadData();
+}
+
+document.addEventListener('DOMContentLoaded', onPageLoaded);
